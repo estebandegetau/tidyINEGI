ae57eab561d60bc15c1196c28033ee1d1c1dfc80
estebandegetau <estebandegetau@gmail.com>
2024-01-31 08:53:28 -0600

    removed handle_numeric

--- R/cleaning_data.R
+++ R/cleaning_data.R
@@ -5,7 +5,7 @@
 #' @return TRUE if `x` holds only one value, other than `NA`.
 is_single_value <- function(x) {
 
-  if(is(x, "factor") | is(x, "numeric")) return(FALSE)
+  if(is(x, "factor") | is(x, "numeric")) {return(FALSE)}
 
   uniq_vals <- x |>
     na.omit() |>
@@ -96,113 +96,6 @@ handle_dichotomic <- function(data) {
 
 }
 
-
-
-
-#' Is an ENIGH variable numeric
-#'
-#' @inheritParams is_dichotomic
-#'
-#' @return TRUE if `x` is numeric
-is_numeric <- function(x) {
-
-  if(is(x, "factor")) {return(FALSE)}
-
-
-  as_numeric <- x |>
-    na.omit() |>
-    as.numeric()
-
-  if(any(is.na(as_numeric))) {return(FALSE)}
-
-  max_vec <- x |>
-    na.omit() |>
-    as.numeric() |>
-    max()
-
-  vec_length <- x |>
-    # Remove NA's
-    na.omit() |>
-    unique() |>
-    length()
-
-
-  no_nas <- x |>
-    as.numeric() |>
-    is.na() |>
-    sum() == 0
-
-  return(max_vec > 10 | vec_length > 4 | no_nas )
-
-}
-
-is_numeric_quiet <- purrr::quietly(is_numeric)
-
-is_types <- function(x) {
-
-  matches_expr <- x |>
-    stringr::str_detect("[:upper:] \\(") |>
-    sum(na.rm = T)
-
-  return(matches_expr == length(x))
-
-}
-
-#' Turn ENIGH numeric variables into numeric class
-#'
-#' @param data An ENIGH data set
-#' @param data_set The name of the ENIGH data set
-#' @param year Year of ENIGH survey
-#'
-#' @return An ENIGH data set with numeric classes
-#' @export
-handle_numeric <- function(data, data_set, year) {
-
-  # Column types
-  types <- readr::read_csv(
-    list.files(
-      here::here(
-        "data-raw",
-        "enigh",
-        "data",
-        "02_open",
-        year,
-        data_set,
-        "diccionario_de_datos"
-      ),
-      full.names = T
-    ),
-    skip = 0,
-    col_types = "c",
-    col_names = F
-  ) |>
-
-    dplyr::filter(stringr::str_detect(1, "^\\d+$")) |>
-    tidyr::drop_na(X2) |>
-    dplyr::select(tidyselect::where(is_types)) |>
-    dplyr::pull(1) |>
-    stats::na.omit() |>
-    stringr::str_extract("[:upper:]") |>
-    stringr::str_to_lower()
-
-  types <- dplyr::case_when(
-    types %in% c("c", "n") ~ types
-  ) |>
-    stats::na.omit()
-
-
-  numeric_vars <- tibble::tibble(variable = names(data),
-                                 type = types) |>
-    dplyr::filter(type == "n") |>
-    dplyr::pull(numeric_vars)
-
-  data |>
-    dplyr::mutate(dplyr::across(tidyselect::any_of(numeric_vars) &
-        !tidyselect::matches("folio|numren|_hog|_id"),
-      as.numeric
-    ))
-
-}
 
 #' Does a vector contain alphabetic characters?
 #'
--- R/use_data.R
+++ R/use_data.R
@@ -3,6 +3,7 @@
 #' Makes it easy to ship data in a package, replicating what
 #' [usethis::use_data()] does, but for ENIGH data.
 #'
+#' @param data The data to be saved
 #' @param data_set The name of an ENIGH data set
 #' @param year The year of the ENIGH survey
 #'
@@ -17,11 +18,12 @@ use_enigh <- function(data, data_set, year) {
   save(
     version = 3,
     list = final_name,
-    file = here::here("data", str_c(final_name, ".RData")),
+    file = here::here("data", stringr::str_c(final_name, ".RData")),
     compress = "xz"
   )
 
   # Remove from environment
+  print(stringr::str_c("Saved data set ", final_name, " to data/"))
   rm(list = final_name)
 
 
